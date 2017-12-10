70a80ed2def30d1eb739130de62360afd1525b12
Valery Kalashnikov <kalashnikov.valeri@gmail.com>
2017-12-10 22:48:03 +0300

    Add methods to get list or delete payload.

--- payload.go
+++ payload.go
@@ -1,3 +1,18 @@
+/*
+   Copyright (C) 2016 The BlameWarrior Authors.
+   This file is a part of BlameWarrior service.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package hooks
 
 import (
@@ -6,8 +21,8 @@ import (
 
 type Payloads interface {
 	Save(repositoryFullName, payload string) error
-	// ListFor(repositoryFullName string, limit, offset int) ([]string, error)
-	// Delete(repositoryFullName, payload string) error
+	List(repositoryFullName string, limit int) ([]string, error)
+	Delete(repositoryFullName, payload string) error
 }
 
 type PayloadRepository struct {
@@ -19,16 +34,13 @@ func NewPayloadRepository(redisClient *redis.Client) *PayloadRepository {
 }
 
 func (repo *PayloadRepository) Save(repositoryFullName, payload string) (err error) {
-
-	err = repo.redisClient.LPush(repositoryFullName, payload).Err()
-
-	return
+	return repo.redisClient.LPush(repositoryFullName, payload).Err()
 }
 
-// func (repo *PayloadRepository) ListFor(repositoryFullName string, limit, offset int) ([]string, error) {
+func (repo *PayloadRepository) List(repositoryFullName string, limit int64) ([]string, error) {
+	return repo.redisClient.LRange(repositoryFullName, 0, limit).Result()
+}
 
-// }
-
-// func (repo *PayloadRepository) Delete(repositoryFullName, payload string) error {
-
-// }
+func (repo *PayloadRepository) Delete(repositoryFullName, payload string) error {
+	return repo.redisClient.LRem(repositoryFullName, 0, payload).Err()
+}
--- payload_test.go
+++ payload_test.go
@@ -1,3 +1,18 @@
+/*
+   Copyright (C) 2016 The BlameWarrior Authors.
+   This file is a part of BlameWarrior service.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package hooks_test
 
 import (
@@ -32,6 +47,51 @@ func TestSavePayload(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 1, len(val))
 	assert.Equal(t, testPayload, val[0])
+}
+
+func TestListPayload(t *testing.T) {
+	redisClient, teardown := setup()
+
+	defer teardown()
+
+	testRepo := "blamewarrior/test"
+	testPayload := "test_payload"
+
+	err := createTestPayload(redisClient, testRepo, testPayload)
+	require.NoError(t, err)
+
+	payloadRepo := hooks.NewPayloadRepository(redisClient)
+
+	list, err := payloadRepo.List(testRepo, int64(2))
+	require.NoError(t, err)
+	require.Equal(t, 1, len(list))
+	require.Equal(t, testPayload, list[0])
+}
+
+func TestDeletePayload(t *testing.T) {
+	redisClient, teardown := setup()
+
+	defer teardown()
+
+	testRepo := "blamewarrior/test"
+	testPayload := "test_payload"
+
+	err := createTestPayload(redisClient, testRepo, testPayload)
+	require.NoError(t, err)
+
+	payloadRepo := hooks.NewPayloadRepository(redisClient)
+
+	err = payloadRepo.Delete(testRepo, testPayload)
+	require.NoError(t, err)
+
+	val, err := redisClient.LRange(testRepo, 0, 0).Result()
+	require.NoError(t, err)
+
+	assert.Empty(t, val)
+}
+
+func createTestPayload(client *redis.Client, testRepo, testPayload string) error {
+	return client.LPush(testRepo, testPayload).Err()
 }
 
 func setup() (client *redis.Client, teardownFn func()) {
